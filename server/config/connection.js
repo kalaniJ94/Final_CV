c6e319ef2919ffcc46db59072dbf1800424950be
kalaniJ94 <kalaniJ94@gmail.com>
2023-12-07 15:39:54 -0700

    forgot to add a server to the connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI ||  'mongodb://127.0.0.1:27017/cv-DB');
 
 module.exports = mongoose.mongoose.connection;
