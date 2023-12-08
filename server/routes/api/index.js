7452369b4f5d155dce8a461563d62be1160adacb
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-07 18:34:10 -0700

    backend running

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,6 +4,6 @@ const userRoutes = require('./userRoutes');
 const voyageRoutes = require('./voyageRoutes');
 
 router.use('/user', userRoutes);
-router.use('/voyages', thoughtRoutes);
+router.use('/voyages', voyageRoutes);
 
 module.exports = router;
