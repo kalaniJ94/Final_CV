e7921bdfe01ad3fb5042193e4b750b9d615a09cf
alester77 <113203488+alester77@users.noreply.github.com>
2023-12-12 13:54:44 -0700

    just kidding, I think this fixed it

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -49,48 +49,46 @@ const LoginForm = ({ onLogin }) => {
   };
 
   return (
-    <>
-      <div className='logInForm scrollable-container'>
-        <h1>Log In</h1>
-        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-            Something went wrong with your login credentials!
-          </Alert>
+    <div className='logInForm scrollable-container' style={{ maxWidth: '400px', margin: 'auto' }}>
+      <h2>Log In</h2>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your login credentials!
+        </Alert>
 
-          <div className="d-flex flex-row justify-content-between">
-            <Form.Group className='mb-3 mr-2' style={{ flex: 1 }}>
-              <Form.Label htmlFor='email'>Email</Form.Label>
-              <Form.Control
-                type='text'
-                placeholder='Your email'
-                name='email'
-                onChange={handleInputChange}
-                value={userFormData.email}
-                required
-              />
-              <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-            </Form.Group>
+        <Form.Group className='mb-2'>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter email'
+            name='email'
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+            size='sm'
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
 
-            <Form.Group className='mb-3 ml-2' style={{ flex: 1 }}>
-              <Form.Label htmlFor='password'>Password</Form.Label>
-              <Form.Control
-                type='password'
-                placeholder='Your password'
-                name='password'
-                onChange={handleInputChange}
-                value={userFormData.password}
-                required
-              />
-              <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-            </Form.Group>
-          </div>
+        <Form.Group className='mb-2'>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            name='password'
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+            size='sm'
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
 
-          <Button className='loginBtn' disabled={!(userFormData.email && userFormData.password)} type='submit' variant='success'>
-            Submit
-          </Button>
-        </Form>
-      </div>
-    </>
+        <Button className='loginBtn' disabled={!(userFormData.email && userFormData.password)} type='submit' variant='success' size='sm'>
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 };
 
