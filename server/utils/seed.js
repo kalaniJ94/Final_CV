5c78981234aa11c3f1d9a9543e272f6cd62850e1
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-11 19:21:56 -0700

    modularizing seeding data

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -1,6 +1,5 @@
 const connection = require("../config/connection");
 const Planet = require("../models/planet");
-const Voyage = require("../models/voyage");
 const fs = require("fs");
 const path = require("path");
 
@@ -55,24 +54,6 @@ const seedPlanets = [
     image: readImageFile(path.join(__dirname, "../../server/assets/images/planet12.png")),  },
 ];
 
-// seeding planets into voyages
-const seedVoyages = [
-  {
-    title: "Basic Celestial Odyssey",
-    price: 1000000,
-    startDate: new Date("2024-10-01"),
-    endDate: new Date("2024-11-15"),
-    destinations: ["Vulcanis Inferna", "Zephyria", "Coloris Orbis"]
-  },
-  {
-    title: "Premium Galactic Getaway",
-    price: 5000000,
-    startDate: new Date("2025-01-01"),
-    endDate: new Date("2025-02-15"),
-    destinations: ["Coralline Nebulae", "Dulcia Orbita", "Aridius Prime", "Spectra Maximus", "Verdantia Secreto"]
-  },
-];
-
 connection.once("open", async () => {
   console.log("Connected to MongoDB");
 
@@ -89,16 +70,7 @@ connection.once("open", async () => {
       }
     }
     console.log('Seeding completed.');
-    connection.close();
 
-    // seed voyages with references to planets
-    for (const seedVoyage of seedVoyages) {
-      const planets = await Planet.find({});
-      seedVoyage.destinations = planets.map((planet) => planet._id);
-      const newVoyage = new Voyage(seedVoyage);
-      await newVoyage.save();
-      console.log(`Voyage successfully seeded: ${seedVoyage.title}`);
-    }
   } catch (err) {
     console.error('Error during seeding:', err);
     connection.close();
