7d485984d61154d988ef5ed98332a8815aa5066e
kalaniJ94 <kalaniJ94@gmail.com>
2023-12-12 21:21:11 -0700

    maybe voyage route fix

--- server/server.js
+++ server/server.js
@@ -15,11 +15,26 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
+// });
+
+// Serve static files from the Vite build directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Define your API routes here
+// Example: app.use('/api', apiRoutes);
+
+// Catch-all route for SPA
+app.get('*', (req, res, next) => {
+  // Use a regex to exclude API routes or specific paths
+  if (/^\/api/.test(req.path)) {
+    return next();  // Skip to next middleware (which could be error handling or 404)
+  }
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.use(routes);
