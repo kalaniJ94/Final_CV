5a604344c6091e27fc1fa9f09242cd44391feebf
kalaniJ94 <kalaniJ94@gmail.com>
2023-12-12 21:54:54 -0700

    just trying something

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://localhost:3001',
+    //     secure: false,
+    //     changeOrigin: true
+    //   }
+    // }
   }
 })
