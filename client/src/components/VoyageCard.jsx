427b10f30239a4e82d08cf9956d37f2754749e6b
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-12 17:17:19 -0700

    more

--- client/src/components/VoyageCard.jsx
+++ client/src/components/VoyageCard.jsx
@@ -2,11 +2,13 @@ import Card from 'react-bootstrap/Card';
 
 import '../style/voyages.css'
 
+const image = '../assets/images/landingpage.png'
+
 function VoyageCard({title, price, destinations}) {
     return (
     <div className='card-container'>
         <Card className='card-element'>
-        <Card.Img variant="top" src='https://images.unsplash.com/photo-1519677100203-a0e668c924d4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dm95YWdlJTIwY2FyZCUyMGltYWdlc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80' style={{width: '100%', height: '50%'}} />
+        <Card.Img variant="top" src={image} style={{width: '100%', height: '50%'}} />
         <Card.Body className='card-body'>
             <Card.Title className='title'><strong>{title}</strong></Card.Title>
             <Card.Text className='price'>
